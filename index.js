a73ca9398eaf6a0807c73ed4a9b5428e85f62cc6
Ganivada Mouli <vishnumouli0@gmail.com>
2023-07-20 11:43:16 +0530

    created a simple heading using react.createElement using react

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement("h1", { id: "title" }, "Hello there!");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
